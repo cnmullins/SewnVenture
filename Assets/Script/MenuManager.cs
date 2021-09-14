a55944305769ff70f06334d7624c4949142002f5
cnmullins <cnmullins@mail.csuchico.edu>
2021-09-14 08:42:39 -0700

    Functional Main Menu

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,56 @@
+/*
+MenuManager.cs
+Author: Christian Mullins
+Date: 9/11/21
+Summary: Handles all UI logic throughout the Main Menu scene.
+*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _mainMenuUI;
+
+    private GameObject _curMenu;
+
+    private void Start()
+    {
+        _curMenu = _mainMenuUI;
+    }
+
+    /// <summary>
+    /// While in Main Menu, this is a way to toggle between menus.
+    /// </summary>
+    /// <param name="menuGO">Root empty GO that holds a menu.</param>
+    public void FocusMenu(GameObject menuGO)
+    {
+        if (_curMenu.Equals(menuGO)) return;
+
+        _curMenu.SetActive(false);
+        menuGO.SetActive(true);
+        _curMenu = menuGO;
+    }
+
+    /// <summary>
+    /// Simple way to quit out of the game.
+    /// </summary>
+    public void QuitGame()
+    {
+        //save data if necessary
+        print("Quitting game...");
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Move to a scene and leave the previous scene to be destroyed.
+    /// </summary>
+    /// <param name="sceneName">Name of scene in build index.</param>
+    public static void MoveToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+}
