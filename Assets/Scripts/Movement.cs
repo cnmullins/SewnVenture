035735eca5bb05a636a7a64d0d7c877c1ee34c68
adamzer0 <noobertrulz@gmail.com>
2021-09-21 09:58:11 -0700

    shop V1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,16 +15,20 @@ public class Movement : MonoBehaviour
     public Vector3 savedpos;
 
     public LayerMask laymask;
+    public LayerMask onlyground;
+    public LayerMask currentlayermask;
     public LayerMask noground;
 
     public bool sewing;
     public GameObject overlay;
+
+    public int thread;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentlayermask = laymask;
     }
 
     // Update is called once per frame
@@ -33,6 +37,8 @@ public class Movement : MonoBehaviour
         Debug.DrawLine(transform.position,transform.forward);
         if (!sewing)
         {
+            //basic movement, it checks if you can move and also checks if you will fall off.
+            //if you wont then you move.
             if (Input.GetKey("w") && !Physics.BoxCast(transform.position, new Vector3(0.5f, 0, 0), Vector3.forward, transform.rotation, 0.6f) && Physics.Raycast(transform.position + Vector3.forward * 0.4f, Vector3.down, 1.5f))
             {
                 transform.position += Vector3.forward * Time.deltaTime * speed;
@@ -50,6 +56,7 @@ public class Movement : MonoBehaviour
                 transform.position += Vector3.right * Time.deltaTime * speed;
             }
         }
+        //holding space lets you cut red strings.
         if (Input.GetKeyDown("space"))
         {
             transform.tag = "Cut";
@@ -58,10 +65,12 @@ public class Movement : MonoBehaviour
         {
             transform.tag = "Untagged";
         }
-
+        //presing R toggles sewing mode.
+        //you can only exit sewing mode if you arent holding any blocks.
+        //you can only enter sewing mode while on a stable surface
         if (Input.GetKeyDown("r") && !sewing)
         {
-            if (!Physics.BoxCast(transform.position, new Vector3(0.5f, 0.1f, 0.5f), Vector3.down, transform.rotation, 2f, laymask))
+            if (!Physics.BoxCast(transform.position, new Vector3(0.5f, 0.1f, 0.5f), Vector3.down, transform.rotation, 2f, currentlayermask))
             {
                 sewing = true;
                 overlay.gameObject.SetActive(true);
@@ -77,13 +86,19 @@ public class Movement : MonoBehaviour
                 myhit = null;
             }    
         }
+        //this is the main raycast, it goes towards the mouse.
+        //it detects what blocks are being hit and lets you move them
+        //the layermask will swap when a block is being held for better movement.
+
         RaycastHit hit;
         Ray ray = (mycam.ScreenPointToRay(Input.mousePosition));
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, laymask) && sewing)
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, currentlayermask) && sewing)
         {
             if (!holdblock)
             {
+                //mousing on or off of a block will make it red and set it up as myhit
+                //this allows you to pick it up.
                 if (myhit != hit.transform.gameObject && myhit != null)
                 {
 
@@ -97,11 +112,15 @@ public class Movement : MonoBehaviour
                     hit.transform.gameObject.GetComponent<MeshRenderer>().material = mat;
 
                 }
-                if (myhit != null)
+                if (myhit != null )
                 {
-                    if (Input.GetMouseButtonDown(0))
+                    if (Input.GetMouseButtonDown(0) && myhit.tag == "Moveable")
                     {
-                       
+                        //when you click on a movable object in sewing mode
+                        //the game will change it to being moved
+                        //it resets the material, change the layer
+                        //and moves the object to the mouses position.
+                        //it then sets the layermask for easier movement
                         if (myhit.transform.GetChild(0).gameObject != null)
                         {
                             detector = myhit.transform.GetChild(0).gameObject;
@@ -110,7 +129,7 @@ public class Movement : MonoBehaviour
                             myhit.layer = 6;
                             holdblock = true;
                             myhit.transform.position += (myhit.transform.position - detector.transform.position);
-
+                            currentlayermask = onlyground;
                         }
                         else
                         {
@@ -119,12 +138,44 @@ public class Movement : MonoBehaviour
                         }
 
                     }
+                    //clicking a shopitem you can afford is interesting
+                    //it removes the currency then just set the object to the one being held
+                    //the game will change it to being moved
+                    //it resets the material, change the layer
+                    //and moves the object to the mouses position.
+                    //it then changes the layermask for easier movement.
+                    if ((Input.GetMouseButtonDown(0) && myhit.tag == "ShopItem") && thread >= myhit.GetComponent<ShopItem>().cost)
+                    {
+                            thread -= myhit.GetComponent<ShopItem>().cost;
+                            myhit.GetComponent<MeshRenderer>().material = mat2;
+                            myhit = Instantiate(myhit.GetComponent<ShopItem>().Purchase,transform.position,transform.rotation);
+                            detector = myhit.transform.GetChild(0).gameObject;
+                            myhit.GetComponent<MeshRenderer>().material = mat2;
+                            savedpos = myhit.transform.position;
+                            myhit.layer = 6;
+                            holdblock = true;
+                            myhit.transform.position += (myhit.transform.position - detector.transform.position);
+                            currentlayermask = onlyground;
+                    }
+                }
+                //if you mouse over a shopitem it becomes red
+                //it also sets it up as "myhit" so clicking will work.
+                if (hit.transform.tag == "ShopItem" && myhit == null)
+                {
+                    myhit = hit.transform.gameObject;
+                    mat2 = myhit.GetComponent<MeshRenderer>().material;
+                    hit.transform.gameObject.GetComponent<MeshRenderer>().material = mat;
+
                 }
             }
+            //if your holding an object, clicking will try to put it down
+            //if it wont fit it wont be put down
+            //if it is successfully put down the layermask will update.
             else if (myhit != null)
             {
                 if (hit.transform.gameObject.layer != 7)
                 {
+
                     myhit.transform.position = new Vector3(Mathf.Round(hit.point.x), (hit.point.y), Mathf.Round(hit.point.z)) + ((myhit.transform.localScale.y / 2) * Vector3.up) + (myhit.transform.position-detector.transform.position); 
                     if (Input.GetMouseButtonDown(0))
                     {
@@ -134,19 +185,34 @@ public class Movement : MonoBehaviour
                             holdblock = false;
                             myhit.transform.position = detector.transform.position;
                             detector = null;
+                            currentlayermask = laymask;
                         }
                     }
                 }
 
             }
-            // Do something with the object that was hit by the raycast.
+           
         }
+        //right clicking will do one of two things.
+        //if its a shop item, it will be sold back to the shop.
+        //if its a non-shop item it will be put back where it used to be.
         if (Input.GetMouseButtonDown(1) && myhit != null && holdblock)
         {
-            myhit.transform.position = savedpos;
-            detector = null;
-            myhit.layer = 7;
-            holdblock = false;
+            if (myhit.GetComponent<Blocks>().cost == 0)
+            {
+                myhit.transform.position = savedpos;
+                detector = null;
+                myhit.layer = 7;
+                holdblock = false;
+            }
+            else
+            {
+                thread += myhit.GetComponent<Blocks>().cost;
+                detector = null;
+                holdblock = false;
+                Destroy(myhit.gameObject);
+            }
+            currentlayermask = laymask;
         }
         /*else if (myhit != null && !holdblock)
         {
@@ -156,5 +222,15 @@ public class Movement : MonoBehaviour
 
 
     }
-   
+    //picking up a token gives you the tokens.
+    //simple.
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Token")
+        {
+            thread += other.GetComponent<Token>().worth;
+            Destroy(other.gameObject);
+        }
+    }
+
 }
---
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopItem : MonoBehaviour
+{
+    public int cost = 0;
+    public GameObject Purchase;
+}
