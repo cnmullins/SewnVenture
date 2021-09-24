52568484744e1c0d9abd8b3ab969c75261ea83b5
cnmullins <cnmullins@mail.csuchico.edu>
2021-09-23 17:14:47 -0700

    Dialogue and Hint UI Events added

---
+++ Assets/Scripts/UI/DialogueController.cs
@@ -0,0 +1,44 @@
+/*
+DialogueController.cs
+Author: Christian Mullins
+Date: 9/21/21
+Summary: Controls dialogue UI that will be manipulate for text.
+*/
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueController : MonoBehaviour
+{
+    public static DialogueController instance; //singleton
+    [SerializeField]
+    private GameObject _hintGO;
+    [SerializeField]
+    private GameObject _dialogueGO;
+
+    private Text _textObj;
+
+    public GameObject dialogueGO => _dialogueGO;
+    public GameObject hintGO => _hintGO;
+    public bool isTriggered { get; private set; }
+
+    private void Start()
+    {
+        instance = this;
+        isTriggered = false;
+        _textObj = _dialogueGO.GetComponentInChildren<Text>();
+    }
+
+    //use for displaying hint/thought text
+    public void SetText(string textStr)
+    {
+        _dialogueGO.SetActive(true);
+        _textObj.text = textStr;
+    }
+
+    //use for displaying dialogue
+    public void SetDialogue(string speakerName, string dialogueStr)
+    {
+        _dialogueGO.SetActive(true);
+        _textObj.text = "<b>" + speakerName + "</b>: " + dialogueStr;
+    }
+}
