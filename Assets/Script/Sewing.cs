38134dc94dc32d2ac510b8ef43779ef21aa91789
adamzer0 <54262635+adamzer0@users.noreply.github.com>
2021-09-27 21:29:28 -0700

    stuff

---
+++ Assets/Script/Sewing.cs
@@ -0,0 +1,31 @@
+/*
+Sewing.cs
+Author: Christian Mullins
+Date: 9/14/21
+Summary: Main script that will handle sewing and unsewing for the player.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sewing : MonoBehaviour
+{
+    private bool _isSewing;
+    private bool _isUnsewing;
+
+    void Start()
+    {
+        
+    }
+
+    public void EnterSewMode()
+    {
+
+    }
+
+    public void EnterUnsewMode()
+    {
+
+    }
+
+}
