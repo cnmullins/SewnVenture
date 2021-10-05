6b2b05b9a95be48e1dc617cf4ae30266da762ae1
adamzer0 <noobertrulz@gmail.com>
2021-10-05 09:51:46 -0700

    a

--- Assets/Scripts/Shop/ShopItem.cs
+++ Assets/Scripts/Shop/ShopItem.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopItem : MonoBehaviour
 {
     public int cost = 0;
     public GameObject Purchase;
+    public TextMeshPro mytext;
+    public void Start()
+    {
+        mytext.text = "" + cost;
+    }
 }
