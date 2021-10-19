25ea7c9bb0ff4fceba9fa6cf33d05b3f044af5ca
adamzer0 <noobertrulz@gmail.com>
2021-10-19 09:43:02 -0700

    silverfish prefab

--- Assets/Scripts/Enemies/Silverfish.cs
+++ Assets/Scripts/Enemies/Silverfish.cs
@@ -47,7 +47,7 @@ public class Silverfish : MonoBehaviour
                 
             
         }
-        if (!Physics.BoxCast(transform.position, new Vector3(0.25f, 0.01f, 0.01f), transform.forward, transform.rotation, Time.deltaTime * speed+0.5f, blockmask) && Physics.Raycast(transform.position + transform.forward * (Time.deltaTime * speed + 0.4f), transform.up*-0.1f, 0.3f, walkmask))
+        if (!Physics.BoxCast(transform.position, new Vector3(0.25f, 0.01f, 0.01f), transform.forward, transform.rotation, Time.deltaTime * speed+0.5f, blockmask) && Physics.Raycast(transform.position + transform.forward * (Time.deltaTime * speed + 0.5f), transform.up*-0.1f, 0.3f, walkmask))
         {
             transform.position += transform.forward * Time.deltaTime * speed;
         }
